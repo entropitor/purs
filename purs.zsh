36fb4ef2bc4a83e7a3c3bcc5afe3010f6b33a2c9
Jens Claes <jens@datacamp.com>
2019-09-10 14:18:36 +0200

    Add $PREPROMPT variable

--- purs.zsh
+++ purs.zsh
@@ -1,7 +1,7 @@
 $(cd $(dirname ${BASH_SOURCE[0]:-${(%):-%x}}) && cargo build --release 2> /dev/null)
 
 function zle-line-init zle-keymap-select {
-  PROMPT=`$(dirname ${BASH_SOURCE[0]:-${(%):-%x}})/target/release/purs prompt -k "$KEYMAP" -r "$?" --venv "${${VIRTUAL_ENV:t}%-*}"`
+  PROMPT="$PREPROMPT `$(dirname ${BASH_SOURCE[0]:-${(%):-%x}})/target/release/purs prompt -k "$KEYMAP" -r "$?" --venv "${${VIRTUAL_ENV:t}%-*}"`"
   zle reset-prompt
 }
 zle -N zle-line-init
